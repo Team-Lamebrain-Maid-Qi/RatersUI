b1ae93d0e82c274353f6c36ae7beb306a092be64
barnord <bdarno92@gmail.com>
2021-08-10 17:28:42 -0700

    http testing

---
+++ RatersUI/RatersUI/Talk.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatersUI
+{
+    class Talk
+    {
+        private static string APIUrl = "https://localhost:44392/";
+        public static void LogIn()
+        {
+
+        }
+    }
+}
